f04c0b232daef7756e7aae340197b2385c36cfec
Matteo Savarese <matteo.savarese@ulb.be>
2024-05-29 16:51:20 +0200

    Added data compression functionality

--- Python/VQPCASuite/clustering.py
+++ Python/VQPCASuite/clustering.py
@@ -6,6 +6,8 @@ import time
 from datetime import datetime
 import os
 import h5py
+from .preprocess import Scaler
+from .metrics import ssim, psnr
 
 class vqpca:
 
@@ -118,31 +120,28 @@ class vqpca:
 
             # Monitor iteration time
             st_iter = time.time()
-
             # Matrix of projection errors
             eps_matrix = np.zeros((self.X_.shape[0], self.k_))
             # Perform PCA in each cluster and calculate projection error
             for i in range(self.k_):
-                        
                 # Predict the point using the local PLS model
                 U_scores = self.pca_[i].transform(self.X_)
                 # Reconstruct X
                 X_rec    = self.pca_[i].inverse_transform(U_scores)
                 # Calculate norm of the residual
                 eps_matrix[:,i] = np.sum((self.X_ - X_rec)**2, axis=1)
-
             # Assign each point to the cluster with the lowest residual
             labels_new = np.argmin(eps_matrix, axis=1)
             # Calculate the new centroids
             centroids_new = np.zeros((self.k_, self.X_.shape[1]))
             for i in range(self.k_):
                 centroids_new[i,:] = np.mean(self.X_[labels_new==i,:], axis=0)
-
             # Calculate the change in the centroids
             delta_centroids = np.linalg.norm(centroids_new - self.C_) / np.linalg.norm(self.C_)
             if delta_centroids < self.ctol_:
                 conv = True
-                print('Converged because of the centroids')
+                if verbose:
+                    print('Converged because of the centroids')
 
             # Check convergence on reconstruction error variance
             eps_rec_new = np.min(eps_matrix, axis=1)
@@ -152,7 +151,8 @@ class vqpca:
                 delta_eps = np.abs((self.eps_ - np.mean(eps_rec_new)) / self.eps_)
                 if delta_eps < self.rtol_:
                     conv = True
-                    print('Converged because of reconstruction error variance')
+                    if verbose:
+                        print('Converged because of reconstruction error variance')
 
             # Update PCA objects
             pca_list = []
@@ -190,17 +190,15 @@ class vqpca:
                     print("Centroids variance = ", delta_centroids)
                 print(f"Elapsed time for iteration: {dt_iter} seconds \n \n")
 
-            else:
-                print("Iteration: ", iter)
-                print("Mean squared global reconstruction error = ", self.eps_ )
-
         # Measure time at convergence
         et_global = time.time()
         dt_global = et_global - st_global
-        print(f"Elapsed time for global convergence: {dt_global} seconds")
+        if verbose == True:
+            print(f"Elapsed time for global convergence: {dt_global} seconds")
 
         # Display information
-        print("Obtained reconstruction error = ", self.eps_)
+        if verbose:
+            print("Obtained reconstruction error = ", self.eps_)
 
         return labels_new, self
     
@@ -349,6 +347,129 @@ class vqpca:
             metric = silhouette_score(self.X_, self.labels_)
 
         return metric
+    
+
+# ----------------- For data compression -------------------- #
+import psutil
+import logging
+
+class compressor:
+
+    # Default initializer
+    def __init__(self, method='vqpca'):
+        # Compression method
+        self.method_ = method
+
+    # Function to monitor resources
+    def monitor_resources(self):
+        cpu_usage = psutil.cpu_percent(interval=1)
+        memory_info = psutil.virtual_memory()
+        print(f"CPU usage: {cpu_usage}%")
+        print(f"Memory usage: {memory_info.percent}%")
+
+    # Function to compress the data
+    def fit(self, X, q=0.99, k=5, scale=True, scaling='auto', verbose=False):
+        # Monitor total execution time
+        st = time.time()
+        process = psutil.Process(os.getpid())
+        memory_info_start = process.memory_info()
+        rss_start = memory_info_start.rss / (1024 * 1024 * 1024)  # Convert to GB
+        # Data pre-processing
+        if scale:
+            scaler = Scaler(method=scaling)
+            Xscaled = scaler.fit_transform(X)
+        else:
+            Xscaled = X
+        # Monitor resources
+        if verbose:
+            print("Pre-processing done")
+            time.sleep(2)
+            self.monitor_resources()
+        # Initialize the compressor
+        if self.method_ == 'vqpca':
+            VQPCA = vqpca(Xscaled, k=k, stopping_rule="variance", q=q)
+            # Fit VQPCA
+            VQPCA.fit(verbose=False)
+            # Monitor usage
+            if verbose:
+                print("Compression via VQPCA done")
+                time.sleep(2)
+                self.monitor_resources()
+            # Get compressed data
+            Xrec = VQPCA.reconstruct(Xscaled)
+            if verbose:
+                print("Reconstruction via VQPCA done")
+                time.sleep(2)
+                self.monitor_resources()
+            # Update self
+            self.Xrec_ = Xrec
+            self.X_ = X
+            # End time
+            et = time.time()
+            elapsed_time = et - st
+            logging.info(f"Script finished in {elapsed_time} seconds")
+            # End RAM
+            memory_info_end = process.memory_info()
+            rss_end = memory_info_end.rss / (1024 * 1024 * 1024)  # Convert to GB
+            print(f"Memory usage: {rss_start:.2f} GB -> {rss_end:.2f} GB (RSS)")
+
+        return Xrec
+    
+    def evaluate(self, custom_loss=False):
+
+        '''This function will evaluate several parameters:
+        the compression ratio, the loss in terms of MSE, NMSE,
+        NRMSE, peak signal-to-noise-ratio, structural similarity index,
+        for global and for each column of X'''
+
+        # Global metrics
+        g_mse = mean_squared_error(self.X_, self.Xrec_)
+        # Structural Similarity Index (SSIM)
+        g_ssim = ssim(self.X_, self.Xrec_)
+
+        print("Global MSE = ", g_mse)
+        print("Global SSIM = ", g_ssim)
+
+        # Metrics for each feature
+        l_mse = []
+        l_ssim = []
+        l_psnr = []
+        for i in range(np.shape(self.X_)[1]):
+            print(f" --- Evaluating feature {i} of {np.shape(self.X_)[1]} ---")
+            l_mse.append(mean_squared_error(self.X_[:,i],self.Xrec_[:,i]))
+            l_ssim.append(ssim(self.X_[:,i], self.Xrec_[:,i]))
+            l_psnr.append(psnr(self.X_[:,i], self.Xrec_[:,i]))
+
+        print("Evaluation completed for each feature")
+        
+        # Create a dictionary
+        scores = {}
+        scores['GlobalMSE'] = g_mse
+        scores['GlobalSSIM'] = g_ssim
+        scores["LocalMSE"] = l_mse
+        scores["LocalSSIM"] = l_ssim
+        scores["LocalPSNR"] = l_psnr
+
+        return scores
+
+
+
+
+
+    
+
+            
+
+        
+        
+
+
+
+    
+
+        
+
+
 
 
 
